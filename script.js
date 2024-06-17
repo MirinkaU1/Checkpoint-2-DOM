e19e346b8adc1f7851c81f6f9411cb3f96d58983
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-17 15:43:20 +0000

    reajustement

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const colorBox = document.getElementById('color-box');
-    const button = document.getElementById('change-color-btn');
-    const text = document.getElementById('title');
+document.addEventListener("DOMContentLoaded", function() {
+    var colorBox = document.getElementById('color-box');
+    var button = document.getElementById('change-color-btn');
+    var text = document.getElementById('title');
 
-    function getRandomColor(numRGB){
+    function getRandomColor(numRGB) {
         return Math.floor(Math.random() * (numRGB + 1));
     }
 
-    button.addEventListener("click", () => {
-        const randomColor = `rgb(${getRandomColor(255)} ${getRandomColor(255)} ${getRandomColor(255)})`;
+    button.addEventListener("click", function() {
+        var randomColor = 'rgb(' + getRandomColor(255) + ',' + getRandomColor(255) + ',' + getRandomColor(255) + ')';
         colorBox.style.background = randomColor;
         text.style.color = randomColor;
         button.style.background = randomColor;
     });
 });
 
-// button.addEventListener("click", () => {});
